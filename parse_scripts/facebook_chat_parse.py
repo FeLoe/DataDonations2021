2fa89e18f6c961fc8ed2690878343ebefbf1f1ad
kasperwelbers <kasperwelbers@gmail.com>
2020-02-18 17:03:41 +0100

    added facebook and whatsapp chat parse scripts

---
+++ parse_scripts/facebook_chat_parse.py
@@ -0,0 +1,124 @@
+### This script is largely based on code gratefully copied from  https://github.com/MasterScrat/Chatistics
+
+import json
+import os
+import pandas as pd
+import logging
+from collections import defaultdict
+import glob
+import argparse
+import datetime
+
+logging.basicConfig()
+logging.getLogger().setLevel(logging.INFO)
+log = logging.getLogger(__name__)
+
+
+def main(own_name, file_path, max_exported_messages):
+    print("heeeey")
+    global MAX_EXPORTED_MESSAGES
+    MAX_EXPORTED_MESSAGES = max_exported_messages
+    log.info('Parsing Facebook messenger data...')
+    if len(glob.glob(os.path.join(file_path, '**', '*.json'))) == 0:
+        log.error(f'No input files found under {file_path}')
+        exit(0)
+    if own_name is None:
+        own_name = infer_own_name(file_path)
+    data = parse_messages(file_path, own_name)
+    log.info('{:,} messages parsed.'.format(len(data)))
+    if len(data) < 1:
+        log.info('Nothing to save.')
+        exit(0)
+    log.info('Converting to DataFrame...')
+    
+    
+    df = pd.DataFrame(data, columns = ['date','drop1','other','self','from_self','message','drop2','drop3'])
+    df = df.drop(['drop1','drop2','drop3'], axis=1)
+    df['date'] = [datetime.datetime.fromtimestamp(x) for x in df['date']]
+    print(df)
+
+
+def parse_messages(file_path, own_name):
+    data = []
+    for root, dirs, files in os.walk(file_path):
+        for filename in files:
+            if not filename.endswith('.json'):
+                continue
+            conversation_id = root.split('/')[-1]
+            conversation_with_name = None
+            document = os.path.join(root, filename)
+            with open(document, encoding="utf8") as f:
+                json_data = json.load(f)
+            if "messages" not in json_data or "participants" not in json_data:
+                log.warning(f"Missing messages or participant list in conversation {conversation_id}")
+                continue
+            participants = json_data["participants"]
+            if len(participants) < 2:
+                log.warning(f"User with id {conversation_id} left Facebook, we don't know what their name was.")
+            if len(participants) > 2:
+                log.info("Group chats are not supported yet.")
+                continue
+            for participant in participants:
+                if participant['name'] != own_name:
+                    conversation_with_name = participant['name']
+            if conversation_with_name is None: conversation_with_name = conversation_id
+            for message in json_data["messages"]:
+                timestamp = message["timestamp_ms"] / 1000
+                if "content" in message and "sender_name" in message:
+                    content = message["content"]
+                    content = fix_text_encoding(content)
+                    if "sender_name" in message:
+                        sender_name = message["sender_name"]
+                    else:
+                        sender_name = conversation_id
+                    outgoing = sender_name == own_name
+                    data += [[timestamp, conversation_id, conversation_with_name, sender_name, outgoing, content, '', '']]
+                    if len(data) >= MAX_EXPORTED_MESSAGES:
+                        log.warning(f'Reached max exported messages limit of {MAX_EXPORTED_MESSAGES}. Increase limit in order to parse all messages.')
+                        return data
+    return data
+
+
+def fix_text_encoding(text):
+    """Fixes text encoding, see https://stackoverflow.com/questions/50008296/facebook-json-badly-encoded"""
+    return text.encode('latin1').decode('utf8')
+
+
+def infer_own_name(file_path, min_conversations=2):
+    """Infers own name from multiple conversations by finding the person who participated most in the conversations"""
+    participants_conversation_count = defaultdict(int)
+    num_conversations = 0
+    log.info('Trying to infer own_name from data...')
+    for root, dirs, files in os.walk(file_path):
+        for filename in files:
+            if not filename.endswith('.json'):
+                continue
+            document_path = os.path.join(root, filename)
+            with open(document_path, 'r', encoding="utf8") as f:
+                json_data = json.load(f)
+            if "participants" not in json_data:
+                continue
+            participants = json_data['participants']
+            # only consider conversations between two participants
+            if len(participants) >= 2:
+                num_conversations += 1
+                for p in participants:
+                    participants_conversation_count[p['name']] += 1
+    if num_conversations >= min_conversations and len(participants_conversation_count.keys()) >= 2:
+        own_name = max(participants_conversation_count, key=participants_conversation_count.get)
+        log.info(f'Successfully inferred own-name to be {own_name}')
+        return own_name
+    raise Exception('Could not infer own name from existing converstations. Please provide your username manually with the --own-name argument')
+
+
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('file', help='The whatsapp chat txt file')
+    parser.add_argument('own_name', help='Name of the person owning the device')
+    args = parser.parse_args()
+
+
+    o = main(args.own_name, args.file, 100000)
+    
---
+++ parse_scripts/whatsapp_parse.py
@@ -0,0 +1,200 @@
+### This script is largely based on code gratefully copied from  https://github.com/MasterScrat/Chatistics
+
+import glob
+import logging
+import os
+from collections import defaultdict
+
+import datetime
+import pandas as pd
+import re
+import uuid
+from tqdm import tqdm
+
+import argparse
+
+logging.basicConfig()
+logging.getLogger().setLevel(logging.INFO)
+log = logging.getLogger(__name__)
+
+regex_left = r'[\u0000-\u001F\u0100-\uFFFF]?'
+regex_datetime = r'[^\w]?([0-9./\-]{6,10},?[\sT][0-9:]{5,8}\s?[AP]?M?)[^\w]?\s?[\-\–]?\s'
+regex_right = r'(([^:]+):\s)?(.*)'
+regex_message = re.compile(f'^{regex_left}{regex_datetime}{regex_right}$')
+MAX_EXPORTED_MESSAGES = 1000000
+
+
+def infer_datetime_regex(f_path, max_messages=100):
+    regex_message = re.compile(f'^{regex_left}({regex_datetime}){regex_right}$')
+    patterns = defaultdict(int)
+    with open(f_path, 'r', encoding="utf8") as f:
+        for c, line in enumerate(f):
+            if c == max_messages:
+                break;
+            matches = regex_message.search(line.upper())
+            if matches:
+                pattern = ""
+                first = True
+                last = 0
+                nums = 0
+                for i, l in enumerate(matches.group(1)):
+                    if l in '0123456789':
+                        if first:
+                            pattern += '('
+                            first = False
+                        nums += 1
+                    else:
+                        if nums > 0:
+                            pattern += '[0-9]'
+                            if nums == 4:
+                                pattern += '{4}'
+                            else:
+                                pattern += '{1,2}'
+                            last = len(pattern)
+                            nums = 0
+                        if l in '.*+[]{}()\\|':
+                            pattern += '\\'
+                        pattern += l
+                        if i > 0 and pattern[-2:] in ['AM','PM']:
+                            pattern = pattern[:-2] + '[APap][Mm]'
+                            last = len(pattern)
+                pattern = pattern[0:last] + ')' + pattern[last:]
+                patterns[pattern] += 1
+    if len(patterns) > 0:
+        regex_dt = max(patterns, key=patterns.get)
+        log.debug(f'Datetime regex inferred: {regex_dt}')
+    else:
+        regex_dt = regex_datetime
+    return re.compile(f'^{regex_left}{regex_dt}{regex_right}$')
+
+
+def main(own_name, file_path, max_exported_messages, infer_datetime):
+    global MAX_EXPORTED_MESSAGES
+    MAX_EXPORTED_MESSAGES = max_exported_messages
+    log.info('Parsing Whatsapp data...')
+    files = glob.glob(os.path.join(file_path, '*.txt'))
+    if len(files) == 0:
+        log.error(f'No input files found under {file_path}')
+        exit(0)
+    if own_name is None:
+        own_name = infer_own_name(files)
+    data = parse_messages(files, own_name, infer_datetime)
+    log.info('{:,} messages parsed.'.format(len(data)))
+    if len(data) < 1:
+        log.info('Nothing to save.')
+        exit(0)
+        
+    df = pd.DataFrame(data, columns = ['date','drop1','other','self','from_self','message','drop2','drop3'])
+    df = df.drop(['drop1','drop2','drop3'], axis=1)
+    df['date'] = [datetime.datetime.fromtimestamp(x) for x in df['date']]
+    print(df)
+    #df['platform'] = 'whatsapp'
+    #log.info('Detecting languages...')
+    #df = detect_language(df)
+    ## Export
+    #export_dataframe(df, config['whatsapp']['OUTPUT_PICKLE_NAME'])
+    #log.info('Done.')
+
+
+def parse_messages(files, own_name, infer_datetime):
+    data = []
+    for f_path in files:
+        log.info(f'Reading {f_path}')
+        f_name = os.path.basename(f_path)
+        conversation_id = uuid.uuid4().hex
+        participants = set()
+        conversation_data = []
+        text = None
+        if infer_datetime:
+            regex_message = infer_datetime_regex(f_path)
+        num_lines = sum(1 for _ in open(f_path, 'r', encoding="utf8"))
+        with open(f_path, 'r', encoding="utf8") as f:
+            for line in tqdm(f, total=num_lines):
+                # try to extract meta data from line
+                matches = regex_message.search(line)
+                if matches is None:
+                    if text is None:
+                        # We are expecting the first message but could not successfully parse line
+                        log.debug(f'Not covered: The line "{line.strip()}" in file {f_path} was ignored')
+                    else:
+                        # We are parsing a multi-line message, add whole line to existing messages and continue with next line
+                        text += '\n' + line.strip()
+                    continue
+                groups = matches.groups()
+                if groups[2] is None:
+                    # sender name is missing but otherwise fits the pattern. Assumed to be app info.
+                    log.debug(f'App info: The line "{line.strip()}" in file {f_path} was ignored')
+                    continue
+                try:
+                    # get timestamp of message
+                    new_timestamp = pd.to_datetime(groups[0]).timestamp()
+                except ValueError:
+                    # Datetime could not be parsed
+                    log.error(f'Could not parse datetime {groups[0]}. False match. Assuming multi-line message instead.')
+                    text += '\n' + line.strip()
+                    continue
+                if text:
+                    # Dump previous entry
+                    conversation_data += [[timestamp, conversation_id, '', sender_name, outgoing, text, '', '']]
+                    if len(data) + len(conversation_data) >= MAX_EXPORTED_MESSAGES:
+                        # dismiss current conversation data
+                        log.warning(f'Reached max exported messages limit of {MAX_EXPORTED_MESSAGES}. Increase limit in order to parse all messages.')
+                        return data
+                # set timestamp
+                timestamp = new_timestamp
+                # add other senders to participants
+                sender_name = groups[2]
+                if sender_name != own_name:
+                    participants.add(sender_name)
+                outgoing = sender_name == own_name
+                text = groups[3].strip()
+            # dump last line
+            if text and sender_name:
+                conversation_data += [[timestamp, conversation_id, '', sender_name, outgoing, text, '', '']]
+        # fill conversation_with
+        if len(participants) == 0:
+            conversation_with_name = ''
+        else:
+            conversation_with_name = '-'.join(sorted(list(participants)))
+        for i in range(len(conversation_data)):
+            conversation_data[i][2] = conversation_with_name
+        # add to existing data
+        data.extend(conversation_data)
+    return data
+
+
+def infer_own_name(files, min_conversations=2):
+    """Infers own name from multiple conversations by finding the person who participated most in the conversations"""
+    if len(files) < min_conversations:
+        raise Exception(
+            f'Cannot infer own-name from less than {min_conversations} conversations. Please provide your username manually with the --own-name argument.')
+    participants_conversation_count = defaultdict(int)
+    num_conversations = 0
+    log.info('Trying to infer own_name from data...')
+    for f_path in files:
+        participants = set()
+        with open(f_path, 'r', encoding="utf8") as f:
+            for line in f:
+                matches = regex_message.search(line)
+                if not matches or not matches.group(3):
+                    continue
+                participants.add(matches.group(3))
+        if len(participants) > 1:
+            num_conversations += 1
+            for p in participants:
+                participants_conversation_count[p] += 1
+    if num_conversations >= min_conversations and len(participants_conversation_count.keys()) >= 2:
+        own_name = max(participants_conversation_count, key=participants_conversation_count.get)
+        log.info(f'Successfully inferred own-name to be {own_name}')
+        return own_name
+    raise Exception('Could not infer own name from existing converstations. Please provide your username manually with the --own-name argument')
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('file', help='The whatsapp chat txt file')
+    parser.add_argument('own_name', help='Name of the person owning the device')
+    args = parser.parse_args()
+
+    o = main(args.own_name, args.file, MAX_EXPORTED_MESSAGES, True)
+    
