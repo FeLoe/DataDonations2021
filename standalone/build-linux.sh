375753c71f893992a314bb5ff7f0a438332f7372
damian0604 <d.c.trilling@uva.nl>
2020-03-31 12:56:05 +0200

    addresses #36 for linux build

--- standalone/build-linux.sh
+++ standalone/build-linux.sh
@@ -3,13 +3,13 @@
 
 echo Trying to build...
 
-if pyinstaller --onefile --noconsole --icon "logo.icns" --add-data="logo_small.gif:." --add-data="news_list.csv:." mijnvoetsporen.py ; then
+
+if pyinstaller --onefile --noconsole  --icon "logo.icns" --add-data="logo_small.gif:." --add-data="news_list.csv:." --exclude-module matplotlib --exclude-module zmq --exclude-module pandas --exclude-module numpy  --exclude-module PIL  --exclude-module urllib3 --exclude-module IPython --exclude-module sqlite3 --exclude-module pydoc --exclude-module shelve  mijnvoetsporen.py  ; then
     echo Done!
 else
     echo This did not work, but at least we have a spec file now.
     echo Increasing the recursion limit in spec file
     sed -i '2iimport sys\nsys.setrecursionlimit(5000)' mijnvoetsporen.spec 
     echo Rebuilding based on modified spec file...
-    pyinstaller --onefile --noconsole --icon "logo.icns" --add-data="logo_small.gif:." --add-data="news_list.csv:." mijnvoetsporen.spec
-
+    pyinstaller --onefile --noconsole  --icon "logo.icns" --add-data="logo_small.gif:." --add-data="news_list.csv:." --exclude-module matplotlib --exclude-module zmq --exclude-module pandas --exclude-module numpy  --exclude-module PIL  --exclude-module urllib3 --exclude-module IPython --exclude-module sqlite3 --exclude-module pydoc --exclude-module shelve  mijnvoetsporen.py
 fi
